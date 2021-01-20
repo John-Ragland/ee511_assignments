fafa4ea88165f9d176805126c595d28b452b7d8b
John Ragland <jhrag@uw.edu>
2021-01-20 14:19:35 -0800

    Add load_data() function

--- assignment2/tools.py
+++ assignment2/tools.py
@@ -1,5 +1,6 @@
 import numpy as np
 import sklearn
+import pandas as pd
 
 '''
 Toolbox for Assignment 2 - ee511 - Statistical Learning
@@ -9,4 +10,26 @@ Winter 2021
 '''
 
 def load_data():
-    pass
+    '''
+    load_data() - read data from url and seperate into training, validation
+        and testing
+    
+    Returns
+    -------
+    train : pandas.dataframe()
+        training data set
+    valid : pandas.dataframe()
+        validation data set
+    test : pandas.dataframe()
+        testing data set
+    '''
+    url = 'http://jse.amstat.org/v19n3/decock/AmesHousing.txt'
+
+    df = pd.read_csv(url, sep='\t')
+
+    valid = df[(df['Order'] % 5) == 3]
+    test = df[(df['Order'] % 5) == 4]
+    mask = ((df['Order'] % 5) != 3) & ((df['Order'] % 5) != 4)
+    train = df[mask]
+
+    return train, valid, test
