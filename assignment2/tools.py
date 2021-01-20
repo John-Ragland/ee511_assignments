8ba2c2668a24179a60a75f4acf1e5e3a7eaf068e
John Ragland <jhrag@uw.edu>
2021-01-20 13:00:51 -0800

    initial commit

---
+++ assignment2/tools.py
@@ -0,0 +1,12 @@
+import numpy as np
+import sklearn
+
+'''
+Toolbox for Assignment 2 - ee511 - Statistical Learning
+University of Washington
+Code by John Ragland and Doruk Arisoy
+Winter 2021
+'''
+
+def load_data():
+    pass
