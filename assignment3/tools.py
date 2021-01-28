72e5e88091f246096948710ca066028e1d542c61
Doruk Arisoy <doruk.arisoy@gmail.com>
2021-01-28 10:59:21 -0800

    data processing optimized

--- assignment3/tools.py
+++ assignment3/tools.py
@@ -1,188 +1,101 @@
 from collections import Counter
 import numpy as np
-import itertools
+import sklearn
 
 class Data:
     '''
     Methods
     -------
-
     _calculate_vocab(self)
-
-    _load_categories(self)
 
     _load_data(self, file)
 
-    get_post_vector_count(self, post)
-    
-    get_post_vector_log(self, post)
-
-    get_post_vector_bin(self, post)
+    get_post_vectors(self, post)
 
     Attributes
     ----------
-    test_file : _io.TextIOWrapper
-        test wrapper for testing data
-    train_file : _io.TextIOWrapper
-        train wrapper for training data
-    test_data_ls : list
-        test_data[a][b][c] => a: category, b: vector of a post in that
-        category, c: frequency of the word self.vocab[c] in this post    
-    
-    train_data_count_ls : list
-        train_data_count[a][b][c] => a: category, b: vector of a post in that
-        category, c: frequency of the word self.vocab[c] in this post.
-        Vocabulary is represented using counted method
-    train_data_log_ls : list
-        train_data_log[a][b][c] => a: category, b: vector of a post in that
-        category, c: frequency of the word self.vocab[c] in this post.
-        Vocabulary is represented by log normalized count method
-    train_data_bin_ls : list
-        train_data_bin[a][b][c] => a: category, b: vector of a post in that
-        category, c: frequency of the word self.vocab[c] in this post.
-        Vocabulary is represented by binary activation method
-    categories : list
-        list of the 20 category names. Index corresponds to a index of train
-        and test data.
+    self.vocab_size : int
+        number of words in the vocabulary
+    self.cat_size : int
+        number of categories
 
-    test_data : numpy array
-        shape (n_test_samples, n_features)
-    test_labels : numpy array
-        shape (n_test_samples,)
-
-    train_data_count : numpy array
-        shape (n_train_samples, n_features)
-    train_data_log : numpy array
-        shape (n_train_samples, n_features)
-    train_data_bin : numpy array
-        shape (n_train_samples, n_features)
-    train_labels : numpy array
-        shape (n_samples,)
-
-
-    vocab : list
+    self.vocab : list
         list containing 5000 most frequency words
+    self.catergories : numpy array
+        shape (self.cat_size,)
 
+    self.test_data_bin : numpy array
+        shape (test_lines, self.vocab_size)
+    self.test_data_max : numpy array
+        shape (test_lines, self.vocab_size)
+    self.test_data_log : numpy array
+        shape (test_lines, self.vocab_size) 
+    self.test_labels  : numpy array
+        shape (test_lines,)
+
+    self.train_data_bin : numpy array
+        shape (train_lines, self.vocab_size)
+    self.train_data_max : numpy array
+        shape (train_lines, self.vocab_size)
+    self.train_data_log : numpy array
+        shape (train_lines, self.vocab_size)
+    self.train_labels : numpy array
+        shape (train_lines,)
     '''
-    def __init__(self, test_name, train_name):
-        print('Opening Data Files...')
-        self.test_file = open(test_name, "r")
-        self.train_file = open(train_name, "r")
+    def __init__(self, test_name='data/20ng-test-all-terms.txt', test_lines=7528, 
+                       train_name='data/20ng-train-all-terms.txt', train_lines=11293, 
+                       vocab_size=5000, cat_size=20):
+        self.vocab_size = vocab_size
+        self.cat_size = cat_size
 
         print('Calculating Vocabulary...')
-        self._calculate_vocab()
+        self._calculate_vocab(open(train_name, "r"))
+        
+        print('Loading Testing Data...')
+        self.test_data_bin, self.test_data_max, self.test_data_log, self.test_labels = self._load_data(open(test_name, "r"), test_lines)
 
         print('Loading Categories...')
-        self._load_categories()
-        
-        # TODO change test data vocab representation to correct form
-        print('Loading Testing Data...')
-        _, _, self.test_data_ls = self._load_data(self.test_file)
+        self.catergories = np.unique(self.test_labels)
 
         print('Loading Training Data...')
-        self.train_data_bin_ls, self.train_data_count_ls, self.train_data_log_ls = self._load_data(self.train_file)
-        
-        # Flatten Lists
-        test_data_ls_flat = list(itertools.chain.from_iterable(itertools.chain.from_iterable(self.test_data_ls)))
-        train_data_bin_ls_flat = list(itertools.chain.from_iterable(itertools.chain.from_iterable(self.train_data_bin_ls)))
-        train_data_count_ls_flat = list(itertools.chain.from_iterable(itertools.chain.from_iterable(self.train_data_count_ls)))
-        train_data_log_ls_flat = list(itertools.chain.from_iterable(itertools.chain.from_iterable(self.train_data_log_ls)))
-
-        # Create Reshaped Numpy Arrays
-        self.test_data = np.reshape(np.array(test_data_ls_flat),(int(len(test_data_ls_flat)/5000),5000))
-        self.train_data_bin = np.reshape(np.array(train_data_bin_ls_flat),(int(len(train_data_bin_ls_flat)/5000),5000))
-        self.train_data_count = np.reshape(np.array(train_data_count_ls_flat),(int(len(train_data_count_ls_flat)/5000),5000))
-        self.train_data_log = np.reshape(np.array(train_data_log_ls_flat),(int(len(train_data_log_ls_flat)/5000),5000))
-
-        # Create training and testing labels
-        self.train_labels, self.test_labels = self._get_labels()
-    
-    def _calculate_vocab(self):
+        self.train_data_bin, self.train_data_max, self.train_data_log, self.train_labels = self._load_data(open(train_name, "r"), train_lines)
+            
+    def _calculate_vocab(self, file):
         vocab = []
-        self.train_file.seek(0)
-        for line in self.train_file:
+        file.seek(0)
+        for line in file:
             words = line.split()[1:]
             vocab.extend(words)
 
         cnt = Counter(vocab)
-        self.vocab = [word[0] for word in cnt.most_common(5000)]
+        self.vocab = [word[0] for word in cnt.most_common(self.vocab_size)]
 
-    def _load_categories(self):
-        categories = []
-        self.test_file.seek(0)
-        for line in self.test_file:
-            category = line.split()[0]
-            if category not in categories:
-                categories.append(category)
-            
-        self.categories = categories
-
-    def _load_data(self, file):
-        # create data variables
-        data_count = [[] for _ in range(len(self.categories))]
-        data_log = [[] for _ in range(len(self.categories))]
-        data_bin = [[] for _ in range(len(self.categories))]
+    def _load_data(self, file, num_of_lines):
+        data_max = np.zeros(shape=(num_of_lines, self.vocab_size))
+        data_log = np.zeros(shape=(num_of_lines, self.vocab_size))
+        data_bin = np.zeros(shape=(num_of_lines, self.vocab_size))
+        labels = np.empty(num_of_lines, dtype='U')
         file.seek(0)
-        
+        line = 0
         for post in file:
             words = post.split()
-            category = words[0]
-            
-            # Calculate vectors for different vocab representations
-            vector_count = self.get_post_vector_count(words[1:])
-            vector_log = self.get_post_vector_log(words[1:])
-            vector_bin = self.get_post_vector_bin(words[1:])
+            labels[line] = words[0]
+            data_bin[line], data_max[line], data_log[line] = self.get_post_vectors(words[1:])
+            line += 1
 
-            # Create Data Variabels
-            data_count[self.categories.index(category)].append(vector_count)
-            data_log[self.categories.index(category)].append(vector_log)
-            data_bin[self.categories.index(category)].append(vector_bin)
-        return data_bin, data_count, data_log
+        data_bin = np.reshape(data_bin, (-1, self.vocab_size))
+        data_max = np.reshape(data_max, (-1, self.vocab_size))
+        data_log = np.reshape(data_log, (-1, self.vocab_size))
+        return data_bin, data_max, data_log, labels
 
-    def get_post_vector_count(self, post):
-        vector = [0 for _ in range(len(self.vocab))]
+    def get_post_vectors (self, post):
+        max = np.zeros(self.vocab_size)
+        binary = np.zeros(self.vocab_size)
         for word in post:
             if word in self.vocab:
-                vector[self.vocab.index(word)] += 1
+                index = self.vocab.index(word)
+                max[index] += 1
+                binary[index] = 1
         
-        # implement max(1,td)
-        for k in range(len(vector)):
-            if vector[k] == 0:
-                vector[k] = 1
-        
-        return vector
-
-    def get_post_vector_log(self, post):
-        td = [0 for _ in range(len(self.vocab))]
-        vector = [0 for _ in range(len(self.vocab))]
-        for word in post:
-            if word in self.vocab:
-                td[self.vocab.index(word)] += 1
-        
-        # implement max(1,td)
-        for k in range(len(td)):
-            vector[k] = np.log(1+td[k])
-        
-        return vector
-
-    def get_post_vector_bin(self, post):
-        vector = [0 for _ in range(len(self.vocab))]
-        for word in post:
-            if word in self.vocab:
-                vector[self.vocab.index(word)] = 1
-        return vector
-
-    def _get_labels(self):
-        train_labels = []
-        for k in range(20):
-            for j in range(len(self.train_data_ls[k])):
-                train_labels.append(self.categories[k])
-        train_labels_array = np.array(train_labels)
-        
-        test_labels = []
-        for k in range(20):
-            for j in range(len(self.test_data_bin_ls[k])):
-                test_labels.append(self.categories[k])
-        test_labels_array = np.array(test_labels)
-
-        return train_labels_array, test_labels_array
+        log = np.log(max + 1)
+        return binary, max, log
