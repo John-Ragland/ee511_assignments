ea2edbe3439b95f99c6263acf502eea9c8afd425
Doruk Arisoy <doruk.arisoy@gmail.com>
2021-02-14 22:35:18 -0800

    cleanup and playing around

--- assignment5/tools.py
+++ assignment5/tools.py
@@ -8,17 +8,12 @@ Languages = {'en':'English', 'es':'Spanish', 'pt':'Portuguese', 'gl':'Galician',
 
 class Data():
     def __init__(self):
-        pass
+        check_files_exists()
 
     def load_data(self):
         '''
         load's data from file and formats to relevent format
         '''
-
-        # Check if data files exist
-        if not (os.path.exists('Data/test.tsv') and os.path.exists('Data/train.tsv') and os.path.exists('Data/val.tsv')):
-            raise Exception ('Data files missings, please add "train.tsv", "test.tsv", or "val.tsv" to ./Data')
-
         train_file = 'Data/train.tsv'
         test_file = 'Data/test.tsv'
         val_file = 'Data/val.tsv'
@@ -30,69 +25,60 @@ class Data():
         train.columns = col_names
         test = pd.read_csv(test_file, header=None, sep='\t', quoting=3)
         test.columns = col_names
-        
 
-
-        vocab = self.get_vocab(train)
+        self.get_vocab(train)
 
         # Convert Train, Test, Validation to list of Tensors
-        train_tens = []
-        test_tens = []
-        val_tens = []
-
-        train_ls = train['tweet'].to_list()
-        test_ls = test['tweet'].to_list()
-        val_ls = val['tweet'].to_list()
         print('Converting Training Data...')
-        for k, item in enumerate(train_ls):
-            train_tens.append(stringToTensor(item, vocab))
-            print(k/len(train_ls)*100,end='\r')
+        self.train = self.list_to_tensor(train['tweet'].to_list())
         print('Converting Test Data...')
-        for k, item in enumerate(test_ls):
-            test_tens.append(stringToTensor(item, vocab))
-            print(k/len(test_ls)*100,end='\r')
+        self.test = self.list_to_tensor(test['tweet'].to_list())
         print('Converting Validation Data...')
-        for k, item in enumerate(val_ls):
-            val_tens.append(stringToTensor(item, vocab))
-            print(k/len(val_ls)*100,end='\r')
-
-
-        self.val = val_tens
-        self.train = train_tens
-        self.test = test_tens
-        return
-
+        self.val = self.list_to_tensor(val['tweet'].to_list())
+    
     def get_vocab(self, train):
-        train_list = train.tweet.to_list()
-        train_char = [i for ele in train_list for i in ele]
+        train_char = [i for ele in train.tweet.to_list() for i in ele]
         most_common = Counter(train_char).most_common()
         char_limit = 10
         for k in range(len(most_common)):
-            if most_common[k][1] < 10:
+            if most_common[k][1] < char_limit:
                 break
-            pass
 
         vocab = most_common[:k]
         vocab = [i[0] for i in vocab]
 
-        vocab.insert(0,'<S>')
-        vocab.insert(0,'</S>')
-        vocab.insert(0,'<N>')
-        return vocab
-
-# Based on https://pytorch.org/tutorials/intermediate/char_rnn_classification_tutorial.html
-def stringToTensor(line, vocab):
-    tensor = torch.zeros(len(line)+2, 1, len(vocab))
-    #add start character
-    tensor[0][0][2] = 1
+        vocab.insert(0,'<S>')   # start token
+        vocab.insert(0,'</S>')  # end token
+        vocab.insert(0,'<N>')   # out-of-vocabulary token
+        self.vocab = vocab
     
-    for li, letter in enumerate(line):
-        try:
-            tensor[li+1][0][vocab.index(letter)] = 1
-        #Add not in vocab character
-        except(ValueError):
-            tensor[li+1][0][0] = 1
-            
-    # add end character
-    tensor[-1][0][1] = 1
-    return tensor
+    def list_to_tensor(self, data):
+        tensor = [0]*len(data)
+        for k, item in enumerate(data):
+            tensor[k] = self.string_to_tensor(item)
+            print(k/len(data)*100, end='\r')
+        return tensor
+
+    def string_to_tensor(self, line):
+        # Based on https://pytorch.org/tutorials/intermediate/char_rnn_classification_tutorial.html
+        tensor = torch.zeros(len(line)+2, 1, len(self.vocab))
+        #add start character
+        tensor[0][0][2] = 1
+        
+        for li, letter in enumerate(line):
+            try:
+                tensor[li+1][0][self.vocab.index(letter)] = 1
+            #Add not in vocab character
+            except(ValueError):
+                tensor[li+1][0][0] = 1
+                
+        # add end character
+        tensor[-1][0][1] = 1
+        return tensor
+
+def check_files_exists():
+    # Check if data files exist
+    if not (os.path.exists('Data/test.tsv') and 
+            os.path.exists('Data/train.tsv') and 
+            os.path.exists('Data/val.tsv')):
+        raise Exception ('Data files missings, please add "train.tsv", "test.tsv", or "val.tsv" to ./Data')
