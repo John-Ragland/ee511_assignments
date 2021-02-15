5994cb8b5a9a6481b57da658b568e8cd4f17eef6
Doruk Arisoy <doruk.arisoy@gmail.com>
2021-02-15 12:11:17 -0800

    perplexity working

--- assignment5/tools.py
+++ assignment5/tools.py
@@ -4,41 +4,16 @@ import os
 from collections import Counter
 import torch
 
-Languages = {'en':'English', 'es':'Spanish', 'pt':'Portuguese', 'gl':'Galician', 'eu':'Basque', 'ca':'Catalan', 'fr':'French', 'it':'Italian', 'de':'German'}
-
 class Data():
     def __init__(self):
-        check_files_exists()
+        self.train = load_data('Data/train.tsv')
+        self.val = load_data('Data/val.tsv')
+        self.test = load_data('Data/test.tsv')
+        self.get_vocab()
 
-    def load_data(self):
-        '''
-        load's data from file and formats to relevent format
-        '''
-        train_file = 'Data/train.tsv'
-        test_file = 'Data/test.tsv'
-        val_file = 'Data/val.tsv'
-
-        col_names = ['lan','tweet']
-        val = pd.read_csv(val_file, header=None, sep='\t', quoting=3)
-        val.columns = col_names
-        train = pd.read_csv(train_file, header=None, sep='\t', quoting=3)
-        train.columns = col_names
-        test = pd.read_csv(test_file, header=None, sep='\t', quoting=3)
-        test.columns = col_names
-
-        self.get_vocab(train)
-
-        # Convert Train, Test, Validation to list of Tensors
-        print('Converting Training Data...')
-        self.train = self.list_to_tensor(train['tweet'].to_list())
-        print('Converting Test Data...')
-        self.test = self.list_to_tensor(test['tweet'].to_list())
-        print('Converting Validation Data...')
-        self.val = self.list_to_tensor(val['tweet'].to_list())
-    
-    def get_vocab(self, train):
-        train_char = [i for ele in train.tweet.to_list() for i in ele]
-        most_common = Counter(train_char).most_common()
+    def get_vocab(self):
+        chars = [i for ele in self.train.tweet.to_list() for i in ele]
+        most_common = Counter(chars).most_common()
         char_limit = 10
         for k in range(len(most_common)):
             if most_common[k][1] < char_limit:
@@ -50,35 +25,60 @@ class Data():
         vocab.insert(0,'<S>')   # start token
         vocab.insert(0,'</S>')  # end token
         vocab.insert(0,'<N>')   # out-of-vocabulary token
+        
         self.vocab = vocab
     
-    def list_to_tensor(self, data):
-        tensor = [0]*len(data)
-        for k, item in enumerate(data):
-            tensor[k] = self.string_to_tensor(item)
-            print(k/len(data)*100, end='\r')
-        return tensor
+    def get_perplexity(self):
+        train_freq = get_freq(self.train, self.vocab)
+        val_freq = get_freq(self.val, self.vocab)
+        train_freq = train_freq / train_freq.sum()
+        val_freq = val_freq / val_freq.sum()
+        train_freq[self.vocab.index('<S>')] = 1.0  # this will be zeroed out later
+        return np.exp(-(val_freq * np.log(train_freq)).sum())
 
-    def string_to_tensor(self, line):
-        # Based on https://pytorch.org/tutorials/intermediate/char_rnn_classification_tutorial.html
-        tensor = torch.zeros(len(line)+2, 1, len(self.vocab))
-        #add start character
-        tensor[0][0][2] = 1
-        
-        for li, letter in enumerate(line):
-            try:
-                tensor[li+1][0][self.vocab.index(letter)] = 1
-            #Add not in vocab character
-            except(ValueError):
-                tensor[li+1][0][0] = 1
-                
-        # add end character
-        tensor[-1][0][1] = 1
-        return tensor
+def load_data(filename):
+    check_files_exists(filename)
+    data = pd.read_csv(filename, header=None, sep='\t', quoting=3)
+    data.columns = ['lan','tweet']
+    return data
 
-def check_files_exists():
+def get_freq(data, vocab):
+    freq = np.zeros(len(vocab))
+    for line in data.tweet:
+        for char in line:
+            if char in vocab:
+                freq[vocab.index(char)] += 1.0
+            else:
+                freq[vocab.index('<N>')] += 1.0
+        freq[vocab.index('</S>')] += 1.0
+    return freq
+
+def list_to_tensor(data, verbose=False):
+    tensor = [0]*len(data)
+    for k, item in enumerate(data):
+        tensor[k] = string_to_tensor(item)
+        if verbose:
+            print('%d percent complete' % k/len(data)*100, end='\r')
+    return tensor
+
+def string_to_tensor(line, vocab):
+    # Based on https://pytorch.org/tutorials/intermediate/char_rnn_classification_tutorial.html
+    tensor = torch.zeros(len(line)+2, 1, len(vocab))
+    #add start character
+    tensor[0][0][2] = 1
+    
+    for li, letter in enumerate(line):
+        try:
+            tensor[li+1][0][vocab.index(letter)] = 1
+        #Add not in vocab character
+        except(ValueError):
+            tensor[li+1][0][0] = 1
+            
+    # add end character
+    tensor[-1][0][1] = 1
+    return tensor
+
+def check_files_exists(filename):
     # Check if data files exist
-    if not (os.path.exists('Data/test.tsv') and 
-            os.path.exists('Data/train.tsv') and 
-            os.path.exists('Data/val.tsv')):
-        raise Exception ('Data files missings, please add "train.tsv", "test.tsv", or "val.tsv" to ./Data')
+    if not os.path.exists(filename):
+        raise Exception ('Data files missings, please add %s.' % filename)
