d80984a897f096c214ff53e0ed4bb75adc1eaab1
John Ragland <jhrag@uw.edu>
2021-02-16 08:21:54 -0800

    playing with onehot

--- assignment5/tools.py
+++ assignment5/tools.py
@@ -3,9 +3,22 @@ import pandas as pd
 import os
 from collections import Counter
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn.utils.rnn import pack_padded_sequence as pack
 
 Languages = {'en':'English', 'es':'Spanish', 'pt':'Portuguese', 'gl':'Galician', 'eu':'Basque', 'ca':'Catalan', 'fr':'French', 'it':'Italian', 'de':'German'}
-
+lan_id = {
+    'en':0, 
+    'es':1, 
+    'pt':2, 
+    'gl':3, 
+    'eu':4, 
+    'ca':5, 
+    'fr':6, 
+    'it':7, 
+    'de':8
+}
 class Data():
     def __init__(self):
         check_files_exists()
@@ -13,6 +26,18 @@ class Data():
     def load_data(self):
         '''
         load's data from file and formats to relevent format
+        
+        Attributes
+        ----------
+        train : tensor
+        test : tensor
+        val : tensor
+        test_labels : tensor
+        train_labels : tensor
+        val_labels : tensor
+        train_pd : pd.df
+        val_pd : pd.df
+        test_pd : pd.df
         '''
         train_file = 'Data/train.tsv'
         test_file = 'Data/test.tsv'
@@ -26,15 +51,33 @@ class Data():
         test = pd.read_csv(test_file, header=None, sep='\t', quoting=3)
         test.columns = col_names
 
+        #save pandas dfs
+        self.train_pd = train
+        self.val_pd = val
+        self.test_pd = test
+
         self.get_vocab(train)
 
         # Convert Train, Test, Validation to list of Tensors
         print('Converting Training Data...')
-        self.train = self.list_to_tensor(train['tweet'].to_list())
+        trainls = self.list_to_tensor(train['tweet'].to_list())
+        print('     Stacking Tensor...', end='\r')
+        self.train = torch.cat(trainls, dim=1)
         print('Converting Test Data...')
-        self.test = self.list_to_tensor(test['tweet'].to_list())
+        testls = self.list_to_tensor(test['tweet'].to_list())
+        print('     Stacking Tensor...', end='\r')
+        self.test = torch.cat(testls, dim=1)
         print('Converting Validation Data...')
-        self.val = self.list_to_tensor(val['tweet'].to_list())
+        valls = self.list_to_tensor(val['tweet'].to_list())
+        print('     Stacking Tensor...', end='\r')
+        self.val = torch.cat(valls, dim=1)
+
+        # Convert Labels to Tensors
+        self.test_labels = self.label_to_tensor(test['lan'].to_numpy())
+        self.train_labels = self.label_to_tensor(train['lan'].to_numpy())
+        self.val_labels = self.label_to_tensor(val['lan'].to_numpy())
+
+        print('Complete')
     
     def get_vocab(self, train):
         train_char = [i for ele in train.tweet.to_list() for i in ele]
@@ -61,7 +104,7 @@ class Data():
 
     def string_to_tensor(self, line):
         # Based on https://pytorch.org/tutorials/intermediate/char_rnn_classification_tutorial.html
-        tensor = torch.zeros(len(line)+2, 1, len(self.vocab))
+        tensor = torch.zeros(161, 1, len(self.vocab))
         #add start character
         tensor[0][0][2] = 1
         
@@ -76,9 +119,47 @@ class Data():
         tensor[-1][0][1] = 1
         return tensor
 
+    def label_to_tensor(self,labels):
+        '''
+        labels : list
+            list of labels for data
+        '''
+        labels_int = []
+        for label in labels:
+            labels_int.append(lan_id[label])
+        labels_tensor = torch.tensor(labels_int)
+        onehot = F.one_hot(labels_tensor)
+        return onehot
+
 def check_files_exists():
     # Check if data files exist
     if not (os.path.exists('Data/test.tsv') and 
             os.path.exists('Data/train.tsv') and 
             os.path.exists('Data/val.tsv')):
         raise Exception ('Data files missings, please add "train.tsv", "test.tsv", or "val.tsv" to ./Data')
+
+
+class RNN_lan_class(nn.Module):
+    def __init__(self, vocab_size, hidden_size, output_size, num_lan):
+        super(RNN_lan_class, self).__init__()
+        self.output_size = output_size
+        self.hidden_size = hidden_size
+        self.vocab_size = vocab_size
+
+        # Create Character and Language embeddings (embed size hard set)
+        self.char_embed = nn.Embedding(vocab_size, 14)
+        self.lan_embed = nn.Embedding(num_lan, 5)
+
+    def forward(self, input, hidden):
+        '''
+        input : tuple
+            tuple of [current character, language]
+        hidden : tensor
+            previous hidden layer output
+        '''
+
+        character = self.char_embed(input[0])
+        return character
+
+    def initHidden(self):
+        return torch.zeros(1, self.hidden_size)
